6beb83b1d111d8af213823307e843a8f47fc3ebb
Pauline Cenizal <paulinestyles1@gmail.com>
2023-09-28 17:23:08 -0700

    almost done

--- lab4-template/lab4.js
+++ lab4-template/lab4.js
@@ -1,40 +1,74 @@
 // method to get value by selector
 const $ = selector => document.querySelector(selector);
-const taxRate = .0925; // 9.25
+const isInvalidValue = val => isNaN(val) || val <= 0;
+const taxRate = .0975; // 9.75
+
 let tall = 0;
 let grande = 0;
 let venti = 0;
+let price = 0;
+let tax = 0;
+let totalAmount = 0;
 
-const doAddSTallCup = () => {
-	// TODO
+const doAddTallCup = () => {
+	tall++;
+	$('#tall-number').value = tall;
 	doOrder();
 };
+
 const doRemoveTallCup = () => {
-	// TODO
-	doOrder();
+	if (tall > 0) {
+		tall--;
+		doOrder();
+	}
 };
+
 const doAddGrandeCup = () => {
-	// TODO
+	grande++;
+	$('#grande-number').value = grande;
 	doOrder();
 };
+
 const doRemoveGrandeCup = () => {
-	// TODO
-	
-	doOrder();
+	if (grande > 0) {
+		grande--;
+		doOrder();
+	}
 };
+
 const doAddVentiCup = () => {
-	// TODO
+	venti++;
+	$('#venti-number').value = venti;
 	doOrder();
 };
+
 const doRemoveVentiCup = () => {
-	// TODO
+	if (venti > 0) {
+		venti--;
+		doOrder();
+	}
+};
+
+const doClearAll = () => {
+	tall = 0;
+	grande = 0;
+	venti = 0;
+	price = 0;
+  tax = 0;
+  totalAmount = 0;
 	doOrder();
 };
-const doClearAll = () => {
-	// TODO
-};
+
 const doOrder = () => {
- 	// TODO
+	price = (tall * 3.99) + (grande * 4.50) + (venti * 5.99);
+	tax = price * taxRate;
+	totalAmount = price + tax;
+	$('#price').value = '$' + price;
+	$('#tax-rate').value = '$' + tax;
+	$('#total-amount').value = '$' + totalAmount;
+	$('#tall-number').value = tall;
+  $('#grande-number').value = grande;
+  $('#venti-number').value = venti;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -48,16 +82,20 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 
 	});
-
-	$('#add-tall').addEventListener('click', () => {
-		// TODO
-	});
+	$('#add-tall').addEventListener('click', doAddTallCup);
 	$('#remove-tall').addEventListener('click', doRemoveTallCup);
 	$('#add-grande').addEventListener('click', doAddGrandeCup);
 	$('#remove-grande').addEventListener('click', doRemoveGrandeCup);
 	$('#add-venti').addEventListener('click', doAddVentiCup);
 	$('#remove-venti').addEventListener('click', doRemoveVentiCup);
-
+  
 	$('#clear-button').addEventListener('click', doClearAll);
 	$('#complete-order').addEventListener('click', doOrder);
+	$('#tax-rate').value = tax;
+	$('#price').value = '$' + price;
+	$('#total-amount').value = totalAmount
+	$('#tall-number').value = tall;
+	$('#grande-number').value = grande;
+	$('#venti-number').value = venti;
+
 });
